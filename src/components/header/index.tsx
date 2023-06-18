45cdb1d6accc718d29304c192b3c27f0df4aa741
Hixo <hixohixo32@gmail.com>
2023-06-18 11:58:11 +0200

    init

---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header className="flex w-screen justify-around">
+            <h1>Devfinder</h1>
+            <button></button>
+        </header>
+    )
+}
